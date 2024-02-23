dac2f2401d684841833cbdeae248b96f0e9c18fa
yawar-ejaz <69yawar@gmail.com>
2024-02-23 15:11:39 +0530

    used a loop to print the ExpenseItems

--- vite-project1/src/App.jsx
+++ vite-project1/src/App.jsx
@@ -1,13 +1,22 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { itemName: "coffee", price: 200, location: "Delhi" },
+    { itemName: "shirt", price: 500, location: "Kolkata" },
+    { itemName: "Jeans", price: 1000, location: "Chennai" },
+  ];
 
   return (
     <>
       <h1>My expenses here</h1>
-      <ExpenseItem itemName="coffee" price={200} location="Delhi"/>
-      <ExpenseItem itemName="Shirt" price={500} location="Kolkata"/>
-      <ExpenseItem itemName="Jeans" price={1000} location="Chennai"/>
+      {expenses.map((e) => (
+        <ExpenseItem
+          itemName={e.itemName}
+          price={e.price}
+          location={e.location}
+        />
+      ))}
     </>
   );
 }
